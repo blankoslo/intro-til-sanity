48100a55b9874e9a14c8ae3b8b326c41c670b63a
mathiasnh <mathias.neslow96@gmail.com>
2022-03-22 14:02:22 +0100

    change everything and hope for the best 🙏

--- web/src/App.js
+++ web/src/App.js
@@ -1,55 +1,137 @@
 import { useState, useEffect } from "react";
-import client from "./client";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import imageUrlBuilder from "@sanity/image-url";
 
-const builder = imageUrlBuilder(client);
-function urlFor(source) {
-  return builder.image(source);
-}
+/**
+ * TODO: Utkommenter følgende når du konfigurert client.js slik at den snakker
+ * med ditt Sanity-prosjekt:
+ */
+// import client from "./client";
+
+// Dette brukes for oppgave 2 bonus:
+// import imageUrlBuilder from "@sanity/image-url";
+// const builder = imageUrlBuilder(client);
+// function urlFor(source) {
+//   return builder.image(source);
+// }
+
+/**
+ * Dette er spørringen som brukes til å hente data fra Sanity.
+ *
+ * title, text, imageUrl og image er felter i "ventPage"-skjemaet
+ */
+const query = `*[_type == "ventPage"][0] {
+  <felter i skjema>      
+}`;
 
 function App() {
-  const [title, setTitle] = useState(null);
-  const [allPosts, setAllPosts] = useState(null);
+  // TODO: Bruk dette for oppgave 1:
+  // const [text, setText] = useState("");
+
+  // TODO: Bruk dette for oppgave 2:
+  // const [imageUrl, setImageUrl] = useState("");
+
+  // TODO: Bruk dette for oppgave 2 bonus:
+  // const [allImageData, setAllImageData] = useState(null);
+
+  // TODO: Bruk dette for oppgave 3:
+  // const [allPosts, setAllPosts] = useState(null);
 
   useEffect(() => {
-    client
-      .fetch(
-        `*[_type == "ventPageAdvanced"][0] {
-          title,
-          posts,      
-        }`
-      )
-      .then((data) => {
-        setTitle(data.title);
-        setAllPosts(data.posts);
-      })
-      .catch(console.error);
+    // TODO: Utkommenter følgende når du har importert en klient:
+    // client
+    //   .fetch(query)
+    //   .then((data) => {
+    //     // For oppgave 1, gitt at feltet du har laget heter "text":
+    //     // setText(data.text);
+    //     //
+    //     /**
+    //      *  For oppgave 2, gitt at feltet du henter ut følgende med GROQ-spørringen:
+    //      *  "imageUrl": image.asset->url,
+    //      * */
+    //     //setImageUrl(data.imageUrl)
+    //     //
+    //     // For oppgave 2 bonus, gitt at feltet du har laget for bildet heter "image":
+    //     // setAllImageData(data.image)
+    //     //
+    //     // For oppgave 3, gitt at feltet du har laget for listen over innlegg heter "posts":
+    //     // setAllPosts(data.posts);
+    //   })
+    //   .catch(console.error);
   }, []);
 
   return (
     <div className="App">
       <Header />
       <section className="main-section">
-        <h2 className="title">{title}</h2>
+        <h2 className="title">Ventilasjonskanal</h2>
         <div className="content">
+          {/* TODO: utkommenter dette for oppgave 1
+          <p>
+            {
+              // TODO: legg til teksten fra Sanity her
+            }
+          </p> 
+          */}
+
+          {/* TODO: utkommenter dette for oppgave 2
+          <div className="imageContainer">
+            <strong>Med vanlig url (kjedelig):</strong>
+            <img
+              src={"// TODO: legg inn URLen til bildet her"}
+              className="image"
+            />
+          </div> 
+          */}
+
+          {/* TODO: utkommenter dette for oppgave 2 bonus
+          {imageRef && (
+            <div className="imageContainer">
+              <strong>Med url-builder (gøy):</strong>
+              <img
+                src={
+                  "// TODO: legg inn URLen til bildet her ved hjelp av url-builder"
+                }
+                className="image"
+              />
+              <p className="imageCaption">
+                {
+                  // TODO: legge inn en eventuell caption her
+                }
+              </p>
+            </div>
+          )} 
+          */}
+
+          {/* TODO: utkommenter dette for oppgave 3
           {allPosts &&
             allPosts.map((post, idx) => (
               <div key={idx} className="post">
                 <span className="author">{"Anon:"}</span>
                 <div style={{ padding: "0 8px" }}>
-                  {post.text && <p className="text">{post.text}</p>}
-                  {post.image && (
-                    <div className="imageContainer">
-                      <img src={urlFor(post.image).url()} className="image" />
-                      <p className="imageCaption">{`Caption: ${post.image.caption}`}</p>
-                    </div>
-                  )}
+                  <p className="text">
+                    {
+                      // TODO: legg inn teksten til innlegget her
+                    }
+                  </p>
+                  <div className="imageContainer">
+                    <img
+                      src={
+                        "// TODO: legg inn URLen til bildet til innlegget her"
+                      }
+                      className="image"
+                    />
+                    <p className="imageCaption">
+                      {
+                        // TODO: legge inn en eventuell caption her
+                      }
+                    </p>
+                  </div>
                 </div>
               </div>
-            ))}
+            ))} 
+            */}
         </div>
       </section>
       <Footer />
--- web/src/client.js
+++ web/src/client.js
@@ -1,7 +1,9 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-  projectId: "cpnkjd52", // denne finner du i studio/sanity.json
-  dataset: "production", // eller navnet du (muligens) ga datasettet under oppsettet
-  useCdn: false, // ikke tenk så mye på dette
-});
+/** TODO: utkommenter dette etter å ha installert @sanity/client med npm
+ import sanityClient from "@sanity/client";
+ 
+ export default sanityClient({
+   projectId: "", // denne finner du i studio/sanity.json
+   dataset: "production", // eller navnet du (muligens) ga datasettet under oppsettet
+   useCdn: false, // ikke tenk så mye på dette
+  });
+*/
